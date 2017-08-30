c8e75959fad4d39de3442063adc1604db6376090
SeedBoot <oliboots@gmail.com>
2017-08-30 16:27:30 +0100

    changed some styling

--- includes/foot.php
+++ includes/foot.php
@@ -1,7 +1,7 @@
 <script src="script/script.js"></script>
         <footer>
             <hr />
-            <p>Created by SeedBoot and pals &copy; 2017</p>
+            <p>Created by SeedBoot &copy; 2017</p>
         </footer>
     </body>
 </html>
